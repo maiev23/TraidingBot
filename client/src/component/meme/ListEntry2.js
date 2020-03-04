bf3ef87cdd3980ab854ac4eabe7307cfdfbfbc29
maiev23 <maiev0691@gmail.com>
2020-03-04 19:24:34 +0900

    채결리스트

---
+++ client/src/component/meme/ListEntry2.js
@@ -0,0 +1,34 @@
+import React, { useState,useEffect } from 'react';
+import Entry2 from './Entry2'
+import axios from 'axios'
+const ListEntry2 = (props) => {
+    const [data, setData] = useState('')
+    console.log(data)
+    useEffect(()=>{
+        let token = localStorage.getItem('token')
+        const config = {
+            headers: { "token": `${token}` },
+          }
+        axios.post('http://localhost:4000/meme/list',{'status': props.mode},config)
+            .then(data => {
+                console.log(data)
+                setData(data)
+            }).catch( (err) => {
+                console.log(err)
+            })
+    },[props.mode])
+    if(data===''){
+        return(
+            <div>loding</div>
+        )
+    }
+    return (
+        <div className='scroll2'>
+            {data.data.map((i,index)=>(
+                <Entry2 data={i} key={index} delete={setData} mode={props.mode}/>
+            ))}
+        </div>
+        );
+};
+
+export default ListEntry2;
